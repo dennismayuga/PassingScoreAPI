c467f7838a39f52f564641787df82e8ff8927c3c
Dennis Mayuga <dennismayu@gmail.com>
2023-01-07 17:18:25 -0500

    Adding my first ever created branch to this repo

---
+++ src/main/java/com/vanier/app/mainClass.java
@@ -0,0 +1,10 @@
+package com.vanier.app;
+
+public class mainClass
+{
+    public static void Main(String[] args)
+    {
+        // Display the number of command line arguments.
+        
+    }
+}
