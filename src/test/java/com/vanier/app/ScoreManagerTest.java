648140d53d1a0232c8e76489fe290cfbc79f37bb
snemurmu <snemurmu@gmail.com>
2023-01-08 11:52:47 -0500

    Added test cases.

--- src/test/java/com/vanier/app/ScoreManagerTest.java
+++ src/test/java/com/vanier/app/ScoreManagerTest.java
@@ -1,7 +1,32 @@
 package com.vanier.app;
 
-//import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 
 public class ScoreManagerTest {
+
+
+    @Test
+    public void failingScoreWithEquals()
+    {
+        String passed = ScoreManager.passingScore(3);        
+        assertEquals("False." ,passed);
+    }
+
+    @Test
+    public void passingScoreWith6()
+    {
+        String passed = ScoreManager.passingScore(6);        
+        assertEquals("True." ,passed);
+    }
+
+    @Test
+    public void passingScoreWith10()
+    {
+        String passed = ScoreManager.passingScore(10);        
+        assertEquals("True." ,passed);
+    }
 
 }
