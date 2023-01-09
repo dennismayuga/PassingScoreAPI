ddf2229b7f5669adb35ebd5de9c894b8e2e91405
2295271 <2295271@edu.vaniercollege.qc.ca>
2023-01-08 23:08:52 -0500

    testing git commands

---
+++ src/main/java/com/vanier/app/Main.java
@@ -0,0 +1,8 @@
+package com.vanier.app;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String message = ScoreManager.execute(args);
+        System.out.println(message);
+    }
+}
